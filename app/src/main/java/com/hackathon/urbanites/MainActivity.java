e5cf7778911a0c73f7bcec35c4a826abd9396646
Apurva Jain <apurvajain.kota@gmail.com>
2017-07-26 15:17:13 +0530

    Add files via upload

--- app/src/main/java/com/hackathon/urbanites/MainActivity.java
+++ app/src/main/java/com/hackathon/urbanites/MainActivity.java
@@ -1,13 +1,51 @@
 package com.hackathon.urbanites;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+    //Icon=R.drawable.icon
+    //busDetail=Activity name
+    public void map_notify(View view, int icon, String contentTitle, String contentText, Class ActivityName) throws ClassNotFoundException {
+            NotificationCompat.Builder mBuilder =
+                    new NotificationCompat.Builder(this)
+                            .setSmallIcon(icon)
+                            .setContentTitle(contentTitle)
+                            .setContentText(contentText)
+                            .setPriority(Notification.PRIORITY_MAX);
+            Intent resultIntent = new Intent(this, ActivityName);
+            TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+            stackBuilder.addParentStack(MainActivity.class);
+            stackBuilder.addNextIntent(resultIntent);
+            PendingIntent resultPendingIntent =
+                    stackBuilder.getPendingIntent(
+                            0,
+                            PendingIntent.FLAG_UPDATE_CURRENT
+                    );
+            mBuilder.setContentIntent(resultPendingIntent);
+            int mNotificationId=001;
+            NotificationManager mNotificationManager =
+                    (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            mNotificationManager.notify(mNotificationId, mBuilder.build());
+
+    }
+
+
 }
