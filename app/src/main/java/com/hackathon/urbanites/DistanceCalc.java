7b77692ee932bb8be793ae023506db8abe433e8d
rutviz vyas <rutvizvyas27@gmail.com>
2017-07-27 16:24:28 +0530

    shortest station

--- app/src/main/java/com/hackathon/urbanites/DistanceCalc.java
+++ app/src/main/java/com/hackathon/urbanites/DistanceCalc.java
@@ -1,6 +1,45 @@
 package com.hackathon.urbanites;
 
+import android.location.Location;
+import android.util.Log;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.maps.android.SphericalUtil;
+
 public class DistanceCalc {
 
+    public double distance(Double sourcelat, Double sourcelong, Double destlat, Double destlong)
+    {
+        Log.d("dist12","Slati"+sourcelat);
+        Log.d("dist12","Slong"+sourcelong);
+        Log.d("dist12","Dlati"+destlat);
+        Log.d("dist12","Dlon"+destlong);
+        double temp1 =  (destlat-sourcelat);
+        double temp2 =  (destlong-sourcelong);
+        Log.d("dist12",temp1+" temp1");
+        Log.d("dist12",temp2+" temp2");
+        temp1*=temp1;
+        temp2*=temp2;
+        Log.d("dist12",temp1+" temp1");
+        Log.d("dist12",temp2+" temp2");
+        Log.d("dist12",Math.sqrt(temp1+temp2)+"");
+        return Math.sqrt(temp1+temp2);
 
+    }
+/*
+    public final static double AVERAGE_RADIUS_OF_EARTH_KM = 6371;
+    public long distance(double userLat, double userLng,
+                         double venueLat, double venueLng) {
+
+        double latDistance = Math.toRadians(userLat - venueLat);
+        double lngDistance = Math.toRadians(userLng - venueLng);
+
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(userLat)) * Math.cos(Math.toRadians(venueLat))
+                * Math.sin(lngDistance / 2) * Math.sin(lngDistance / 2);
+
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        return (Math.round(AVERAGE_RADIUS_OF_EARTH_KM * c));
+    }*/
 }
--- app/src/main/java/com/hackathon/urbanites/MainActivity.java
+++ app/src/main/java/com/hackathon/urbanites/MainActivity.java
@@ -4,6 +4,8 @@ import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -38,11 +40,21 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
 
 import Modules.DirectionFinder;
+import Modules.Distance;
+import Modules.MyLocation;
 import Modules.DirectionFinderListener;
 import Modules.Route;
 
@@ -63,9 +75,18 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private List<Polyline> polylinePaths = new ArrayList<>();
     private ProgressDialog progressDialog;
     private int temp;
+    private ArrayList<MyLocation> RMTS_bus;
+    private double SourceLat;
+    private double SourceLong;
+    private double DestLat;
+    private double DestLong;
+    private DistanceCalc DC = new DistanceCalc();
+
 
 
     EditText Source, Destination;
+
+    LatLng NearSource, NearDest;
 
 
     @Override
@@ -79,6 +100,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        RMTS_bus = loadJSONFromAsset();
 
         Source.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -109,6 +132,55 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             }
         });
 
+    }
+
+    private ArrayList<MyLocation> loadJSONFromAsset() {
+        Resources res = getResources();
+
+        InputStream is = res.openRawResource(R.raw.rajkot_bus_list);
+        Scanner scanner = new Scanner(is);
+        StringBuilder builder = new StringBuilder();
+        while(scanner.hasNextLine())
+        {
+            builder.append(scanner.nextLine());
+        }
+        return parseJson(builder.toString());
+    }
+
+    private ArrayList<MyLocation> parseJson(String s) {
+        ArrayList<MyLocation> locList = new ArrayList<>();
+        StringBuilder builder = new StringBuilder();
+        try {
+            JSONObject root = new JSONObject(s);
+
+            JSONArray RMTS = root.getJSONArray("RMTS");
+            for(int i=0;i<RMTS.length();i++)
+            {
+                Log.d("Station",RMTS.length()+"");
+                JSONObject routes = RMTS.getJSONObject(i);
+                JSONArray route = routes.getJSONArray("Route"+(i+1));
+                for (int j=0;j<route.length();j++)
+                {
+                    Log.d("Station",route.length()+"");
+                    JSONObject val = route.getJSONObject(j);
+                    MyLocation location = new MyLocation();
+                    location.setName(val.getString("name"));
+                    location.setId(val.getInt("id"));
+                    location.setLongitude(val.getDouble("long"));
+                    location.setLatitude(val.getDouble("lat"));
+                    locList.add(location);
+                }
+            }
+
+        } catch (JSONException e) {
+            Log.d("Station","error");
+            builder.append("name: ");
+            e.printStackTrace();
+            Log.d("test",e.getMessage());
+        }
+
+        Log.d("test",builder.toString());
+        return locList;
     }
 
     private void sendRequest() {
@@ -161,10 +233,14 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             if (resultCode == RESULT_OK) {
                 Place place = PlaceAutocomplete.getPlace(this, data);
                 Source.setText(place.getName() + ", " + place.getAddress());
-                if (!Destination.getText().toString().equals("")){
+                SourceLat = place.getLatLng().latitude;
+                SourceLong = place.getLatLng().longitude;
+                Log.d("test",place.getLatLng().latitude+"");
+                Log.d("test",place.getLatLng().longitude+"");
+                /*if (!Destination.getText().toString().equals("")){
                     Log.d(TAG,"Finding");
                     sendRequest();
-                }
+                }*/
                 Log.i(TAG, "Place: " + place.getName());
             } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
                 Status status = PlaceAutocomplete.getStatus(this, data);
@@ -176,9 +252,18 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             if (resultCode == RESULT_OK) {
                 Place place = PlaceAutocomplete.getPlace(this, data);
                 Destination.setText(place.getName()+", "+ place.getAddress());
+                DestLat = place.getLatLng().latitude;
+                DestLong = place.getLatLng().longitude;
+
+                Log.d("test",place.getLatLng().latitude+"");
+                Log.d("test",place.getLatLng().longitude+"");
                 if (!Source.getText().toString().equals("")){
                     Log.d(TAG,"Finding");
                     sendRequest();
+                    double dist = getNearestBRTS();
+                    Log.d("distance", String.valueOf(dist));
+
+
                 }
                 Log.i(TAG, "Place: " + place.getName() + ", " + place.getAddress());
 
@@ -189,8 +274,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             }
         }
     }
-
-
 
 
 
@@ -216,6 +299,34 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }*/
     }
 
+    private double getNearestBRTS() {
+        double D1;
+        double D2 = 0;
+        String near_station = null;
+
+        for (MyLocation location : RMTS_bus)
+        {
+            D1 =  DC.distance(SourceLat,SourceLong,location.getLatitude(),location.getLongitude());
+          //  Log.d("Send123","Source-"+SourceLat+" "+SourceLong+" "+location.getLatitude()+" "+location.getLongitude());
+            Log.d("Distance123",location.getName()+"--"+D1);
+            Log.d("Distance123",near_station+"--"+D2);
+            if(D2 == 0)
+            {
+                D2 = D1;
+                near_station = location.getName();
+            }
+
+            if(D1 < D2)
+            {
+                Log.d("Distance123","changed");
+                D2 = D1;
+                near_station = location.getName();
+            }
+
+        }
+        Log.d("Station",near_station);
+        return D2;
+    }
     @Override
     public void onDirectionFinderSuccess(List<Route> routes) {
         progressDialog.dismiss();
@@ -226,6 +337,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         for (Route route : routes) {
             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(route.startLocation, 12));
+            Log.d("test",route.distance.text);
             /*((TextView) findViewById(R.id.tvDuration)).setText(route.duration.text);
             ((TextView) findViewById(R.id.tvDistance)).setText(route.distance.text);*/
 
@@ -268,6 +380,45 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             }
         }
     }
+
+   /* public ArrayList<MyLocation> loadJSONFromAsset() {
+        ArrayList<MyLocation> locList = new ArrayList<>();
+        String json = null;
+        try {
+            Log.d("outputJson","1 done");
+            InputStream is = getAssets().open("rajkot_bus_list.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+
+        }
+        try {
+            JSONObject obj = new JSONObject(json);
+            JSONArray m_jArry = obj.getJSONArray("RMTS").getJSONArray(1);
+            Log.d("outputJson","1 done");
+            for (int i = 0; i < m_jArry.length(); i++) {
+                JSONObject jo_inside = m_jArry.getJSONObject(i);
+                MyLocation location = new MyLocation();
+                location.setId(jo_inside.getInt("id"));
+                location.setName(jo_inside.getString("name"));
+                location.setLatitude((float) jo_inside.getDouble("lat"));
+                location.setLongitude((float) jo_inside.getDouble("long"));
+                Log.d("outputJson",jo_inside.getString("name"));
+
+                //Add your values in your `ArrayList` as below:
+                locList.add(location);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return locList;
+
+    }*/
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
